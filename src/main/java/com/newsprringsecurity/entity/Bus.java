9f181402399b328e550d5dd97b71f2e93e9ea137
golu9031 <amit.amit.mehta70@gmail.com>
2024-04-02 04:35:46 +0530

    123:My new commit with bus entity

---
+++ src/main/java/com/newsprringsecurity/entity/Bus.java
@@ -0,0 +1,40 @@
+package com.newsprringsecurity.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "buses")
+public class Bus {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 155)
+    private String name;
+    private String busNumber;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBusNumber() {
+        return busNumber;
+    }
+
+    public void setBusNumber(String busNumber) {
+        this.busNumber = busNumber;
+    }
+}
---
+++ src/main/java/com/newsprringsecurity/repository/BusRepository.java
@@ -0,0 +1,7 @@
+package com.newsprringsecurity.repository;
+
+import com.newsprringsecurity.entity.Bus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BusRepository extends JpaRepository<Bus,Long> {
+}
